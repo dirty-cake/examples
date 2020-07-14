7327c27fdbf36b9ed1598f38d2f29bd112ae4e29
karatalexx <karatalexx@gmail.com>
2020-07-14 10:38:18 +0300

    HOC

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ClickCounter from './components/ClickCounter.js';
-import HoverCounter from './components/HoverCounter.js'
+import ClickCounter from './higher order coomponents/ClickCounter.js';
+import HoverCounter from './higher order coomponents/HoverCounter.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ClickCounter />
+    <ClickCounter name='ClickName' />
     <HoverCounter />
   </React.StrictMode>,
   document.getElementById('root')
